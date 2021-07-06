4794507293eb8eef7c5947d3b77123a129e7c54c
GimYoungPhil <youngphil.gim@gmail.com>
2021-07-06 13:57:41 +0900

    fixed

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const { ApolloServer, gql } = require('apollo-server-express');
 require('dotenv').config();
 const db = require('./db');
 
+const jwt = require('jsonwebtoken');
 const models = require('./models');
 
 const port = process.env.PORT || 4000;
@@ -15,11 +16,24 @@ const resolvers = require('./resolvers');
 const app = express();
 db.connect(DB_HOST);
 
+const getUser = token => {
+  if (token) {
+    try {
+      return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+      throw new Error('Sessoin invalid');
+    }
+  }
+};
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: () => {
-    return { models };
+  context: ({ req }) => {
+    const token = req.headers.authorization;
+    const user = getUser(token);
+    console.log(user);
+    return { models, user };
   },
 });
 server.applyMiddleware({ app, path: '/api' });
